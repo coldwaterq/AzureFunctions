50382f39f3b5fa5fd3ce0602d4d203af93118739
coldwaterq <coldwaterq@gmail.com>
2017-03-19 13:52:46 -0700

    This should be working.

--- testfunction/run.py
+++ testfunction/run.py
@@ -1,3 +1,23 @@
 import os
+import json
 
 print(os.environ)
+
+# All data to be returned to the client gets put into this dict
+returnData = {
+    #HTTP Status Code:
+    "status": 200,
+    
+    #Response Body:
+    "body": "<h1>Azure 2 Works :)</h1>",
+    
+    # Send any number of HTTP headers
+    "headers": {
+        "Content-Type": "text/html",
+        "X-Awesome-Header": "YesItIs"
+    }
+}
+
+# Output the response to the client
+output = open(os.environ['res'], 'w')
+output.write(json.dumps(returnData))
